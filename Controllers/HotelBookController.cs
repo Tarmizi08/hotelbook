fb4c43adff9dc287f9a815dbefda291ee028e072
Tarmizi <57948525+Tarmizi08@users.noreply.github.com>
2023-03-16 18:49:35 +0800

    Add project files.

---
+++ Controllers/HotelBookController.cs
@@ -0,0 +1,29 @@
+﻿using hotelbook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hotelbook.Controllers
+{
+    public class HotelBookController : Controller
+    {
+        private static List<HotelBooking> hotelBooked = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HotelBookingDetails() {
+                     
+            return View(hotelBooked);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitForm(HotelBooking hotelBooking)
+        {
+            hotelBooked.Add(hotelBooking);
+            return RedirectToAction(nameof(HotelBookingDetails));
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hotelbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
